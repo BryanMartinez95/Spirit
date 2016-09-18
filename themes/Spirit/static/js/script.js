3822e10efa190b0d7f970c6728a9087901a7ac3b
Bryan Martinez <b.martinez.95>
2016-09-17 21:27:05 -0700

    navbar media update + code cleanup

--- themes/Spirit/static/js/script.js
+++ themes/Spirit/static/js/script.js
@@ -1,11 +1,12 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
     // document.body.style.backgroundColor = "#AA0000 !important";
 //   /  document.getElementById("body").style.backgroundColor = "#f8f8f8 ";
     
     // document.getElementById("content").style.backgroundColor = "rgba(0,0,0,0.4)";
     // document.getElementById("content").style.opacity = "0.5";
     $('.dim').fadeIn(200);
+    $('body').addClass('stop-scrolling');
     event.stopPropagation();
 }
 
@@ -13,14 +14,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     $('.dim').fadeOut(200);
+    $('body').removeClass('stop-scrolling');
     // document.getElementById("content").style.backgroundColor = "white";
 }
 
 $('html').click(function() {
   //Hide the menus if visible
+  $('body').removeClass('stop-scrolling');
   var width= document.getElementById("mySidenav").style.width;
-  console.log("test");
-  console.log(width);
   if(width != "0")
   {
     document.getElementById("mySidenav").style.width = "0";
@@ -34,12 +35,12 @@ $('html').click(function() {
 //     event.stopPropagation();
 // });
 
-$(document).ready(function() {
-  $("ul.opMenu li").click(function(){
-   $('#MainOptSubMenu',this).css('visibility', 'visible');
-  });
+// $(document).ready(function() {
+//   $("ul.opMenu li").click(function(){
+//    $('#MainOptSubMenu',this).css('visibility', 'visible');
+//   });
 
-  $("ul.opMenu li").mouseleave(function(){
-      $('#MainOptSubMenu',this).css('visibility', 'hidden');
-  });
-});
+//   $("ul.opMenu li").mouseleave(function(){
+//       $('#MainOptSubMenu',this).css('visibility', 'hidden');
+//   });
+// });
