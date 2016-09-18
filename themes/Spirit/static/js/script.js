30c5a7a223e0dcd32c33fc3bf998b0db74096b3a
Bryan Martinez <bmartinez@resourceenergysolutions.com>
2016-09-17 19:13:14 -0600

    transfer

--- themes/Spirit/static/js/script.js
+++ themes/Spirit/static/js/script.js
@@ -1,16 +1,33 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    // document.body.style.backgroundColor = "#AA0000 !important";
+//   /  document.getElementById("body").style.backgroundColor = "#f8f8f8 ";
+    
+    // document.getElementById("content").style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.getElementById("content").style.opacity = "0.5";
+    $('.dim').fadeIn(200);
     event.stopPropagation();
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    $('.dim').fadeOut(200);
+    // document.getElementById("content").style.backgroundColor = "white";
 }
 
 $('html').click(function() {
   //Hide the menus if visible
-  document.getElementById("mySidenav").style.width = "0";
+  var width= document.getElementById("mySidenav").style.width;
+  console.log("test");
+  console.log(width);
+  if(width != "0")
+  {
+    document.getElementById("mySidenav").style.width = "0";
+     $('.dim').fadeOut(200);
+  }
+
+//   document.getElementById("content").style.backgroundColor = "white";
 });
 
 // $('#menucontainer').click(function(event){
